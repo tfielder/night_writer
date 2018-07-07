9bbfccc6c74798864dc0418c59eb030e8aa36a4d
Tim Fielder <fiel1139@gmail.com>
2018-07-06 19:10:23 -0600

    finished through iteration 1.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,23 @@
-class NightWriter
-  def initialize(file)
-    message_text = ARGV
-    puts "Night Writer Initialized"
-    puts message_text
-  end
+handle = File.open(ARGV[0], "r")
 
-end
+incoming_text = handle.read
+handle.close
+counted_text = incoming_text.size
+puts "Created '#{ARGV[1]}' containing #{counted_text} characters."
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(counted_text)
+
+writer.close
+
+
+
+# class NightWriter
+#   def initialize(file)
+#     message_text = ARGV
+#     puts "Night Writer Initialized"
+#     puts message_text
+#   end
+
+#end
