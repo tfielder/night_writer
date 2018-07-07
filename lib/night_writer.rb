e2d74ad8a20ba9bc37ded01824ee349934aa35d7
Tim Fielder <fiel1139@gmail.com>
2018-07-06 19:30:02 -0600

    started class of NightWriter.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,18 @@
+require ".braille_legend.rb"
+
+class NightWriter
+  attr_reader :translate
+
+  def initialize
+    @braille_legend =
+  end
+
+  def translate(input)
+    @braille_legend.key(input) == @braille_legend.value
+  end
+
+end
+
 handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
@@ -7,17 +22,10 @@ puts "Created '#{ARGV[1]}' containing #{counted_text} characters."
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(counted_text)
+writer.write(incoming_text)
 
 writer.close
 
 
 
-# class NightWriter
-#   def initialize(file)
-#     message_text = ARGV
-#     puts "Night Writer Initialized"
-#     puts message_text
-#   end
-
-#end
+#
